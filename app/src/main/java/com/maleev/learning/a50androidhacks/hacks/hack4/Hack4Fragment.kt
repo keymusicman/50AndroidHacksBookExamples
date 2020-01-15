5d8234515150df1c6a586a0e3ca972c311ec3de7
Vasily Maleev <keymusicman@gmail.com>
2020-01-16 00:41:47 +0300

    Add bottom line for Hack 4

--- app/src/main/java/com/maleev/learning/a50androidhacks/hacks/hack4/Hack4Fragment.kt
+++ app/src/main/java/com/maleev/learning/a50androidhacks/hacks/hack4/Hack4Fragment.kt
@@ -5,10 +5,17 @@ import androidx.preference.EditTextPreferenceDialogFragmentCompat
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.maleev.learning.a50androidhacks.R
+import com.maleev.learning.a50androidhacks.utils.annotations.HackBottomLineText
 import com.maleev.learning.a50androidhacks.utils.annotations.HackDescription
 import com.maleev.learning.a50androidhacks.utils.annotations.HackNumber
 
 @HackNumber(4)
+@HackBottomLineText(
+    "Although the settings framework allows you to add some custom behavior, you need to " +
+            "remember that its purpose is to create simple preferences screens. If you’re thinking " +
+            "of adding more complex user interfaces or flows, I’d recommend you create a separate Activity, " +
+            "theming it as a Dialog, and launching it from a preferences widget. "
+)
 @HackDescription("Preferences hacks")
 class Hack4Fragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
